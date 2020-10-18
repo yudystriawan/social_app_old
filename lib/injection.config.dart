c71c5c98a04623886d5bfaf69948717d484a82db
yudystriawan <yudistriawan@gmail.com>
2020-10-18 19:46:26 +0800

    implement injection

---
+++ lib/injection.config.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'application/auth/sign_in_form/sign_in_form_bloc.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
+
+GetIt $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) {
+  final gh = GetItHelper(get, environment, environmentFilter);
+  gh.factory<SignInFormBloc>(() => SignInFormBloc());
+  return get;
+}
