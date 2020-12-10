2b8e88ce00ad27ebf4c9144096a245134d09ce17
yudystriawan <yudistriawan@gmail.com>
2020-12-10 14:13:18 +0800

    fix ui

--- lib/presentation/pages/posts/post.dart
+++ lib/presentation/pages/posts/post.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_social_app/application/post/actor/post_actor_bloc.dart';
+import 'package:my_social_app/application/user/get_by_id/user_get_by_id_bloc.dart';
 import 'package:my_social_app/domain/core/value_objects.dart';
 import 'package:my_social_app/domain/post/post.dart';
 import 'package:my_social_app/presentation/common/widgets/my_cached_network_image.dart';
@@ -22,17 +23,50 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<PostActorBloc>(),
-      child: Column(
-        children: [
-          PostHeader(post: post),
-          PostImage(post: post),
-          const SizedBox(height: 8),
-          PostFooter(post: post),
-        ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<PostActorBloc>(),
+        ),
+        BlocProvider(
+          create: (context) => getIt<UserGetByIdBloc>()
+            ..add(UserGetByIdEvent.getUserById(post.userId.getOrCrash())),
+        ),
+      ],
+      child: BlocBuilder<UserGetByIdBloc, UserGetByIdState>(
+        builder: (context, state) {
+          return state.map(
+            initial: (_) => Container(),
+            loadInProgress: (_) => const Center(
+              child: CircularProgressIndicator(),
+            ),
+            loadFailure: (_) => Container(),
+            loadSuccess: (state) => Column(
+              children: [
+                PostHeader(
+                  post: post,
+                  user: state.user,
+                ),
+                PostImage(post: post),
+                const SizedBox(height: 8),
+                PostFooter(post: post),
+              ],
+            ),
+          );
+        },
       ),
     );
+    // return BlocProvider(
+    //   create: (context) => getIt<PostActorBloc>(),
+    //   child: Column(
+    //     children: [
+    //       PostHeader(post: post),
+    //       PostImage(post: post),
+    //       const SizedBox(height: 8),
+    //       PostFooter(post: post),
+    //     ],
+    //   ),
+    // );
   }
 }
 
--- lib/presentation/pages/posts/widgets/post_header_widget.dart
+++ lib/presentation/pages/posts/widgets/post_header_widget.dart
@@ -2,52 +2,37 @@ import 'package:auto_route/auto_route.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flushbar/flushbar_helper.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_social_app/application/user/get_by_id/user_get_by_id_bloc.dart';
 import 'package:my_social_app/domain/post/post.dart';
-import 'package:my_social_app/injection.dart';
+import 'package:my_social_app/domain/user/user.dart';
 import 'package:my_social_app/presentation/routes/router.gr.dart';
 
 class PostHeader extends StatelessWidget {
   const PostHeader({
     Key key,
     @required this.post,
+    @required this.user,
   }) : super(key: key);
 
   final PostDomain post;
+  final UserDomain user;
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<UserGetByIdBloc>()
-        ..add(UserGetByIdEvent.getUserById(post.userId.getOrCrash())),
-      child: BlocBuilder<UserGetByIdBloc, UserGetByIdState>(
-        builder: (context, state) {
-          return state.map(
-            initial: (_) => Container(),
-            loadInProgress: (_) => Container(),
-            loadFailure: (_) => Container(),
-            loadSuccess: (state) => ListTile(
-              leading: CircleAvatar(
-                  backgroundImage: CachedNetworkImageProvider(
-                      state.user.photoUrl.getOrCrash())),
-              title: GestureDetector(
-                onTap: () => ExtendedNavigator.of(context).push(
-                    Routes.profilePage,
-                    arguments: ProfilePageArguments(
-                        userId: state.user.id.getOrCrash())),
-                child: Text(state.user.username.getOrCrash()),
-              ),
-              subtitle: Text(post.location.getOrCrash()),
-              trailing: IconButton(
-                icon: const Icon(Icons.more_vert),
-                onPressed: () => FlushbarHelper.createInformation(
-                  message: 'Delete post.',
-                ).show(context),
-              ),
-            ),
-          );
-        },
+    return ListTile(
+      leading: CircleAvatar(
+          backgroundImage:
+              CachedNetworkImageProvider(user.photoUrl.getOrCrash())),
+      title: GestureDetector(
+        onTap: () => ExtendedNavigator.of(context).push(Routes.profilePage,
+            arguments: ProfilePageArguments(userId: user.id.getOrCrash())),
+        child: Text(user.username.getOrCrash()),
+      ),
+      subtitle: Text(post.location.getOrCrash()),
+      trailing: IconButton(
+        icon: const Icon(Icons.more_vert),
+        onPressed: () => FlushbarHelper.createInformation(
+          message: 'Delete post.',
+        ).show(context),
       ),
     );
   }
